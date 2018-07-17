e193f428830ed79254dd306a3b5dfda637120e37
Tpk <admin@tcgforum.pl>
2018-07-17 16:55:34 +0200

    Add MongoDB driver

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv').config()
 
 const Router = require('./routes/router.js')
 
 // Basic configuration
 const app = express();
-const port = process.env.APP_PORT || 8080
+const port = process.env.APP_PORT || 8080;
+const db = mongoose.connection;
 
-// Setup router
-app.use(Router)
+// Connect to Mongo
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+
+    console.log('Mongo established connection')
+
+    // Setup router
+    app.use(Router)
+
+});
 
 // Run server
 app.listen(port, () => console.log(`Server started on ${port}`));
